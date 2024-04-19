c6dfab6f219ca5e3c218f5e697ab43a40465670c
Eddie Tenz <tzz1002@gmail.com>
2024-04-20 00:44:38 +0800

    cache example

--- cache/example/main.go
+++ cache/example/main.go
@@ -60,6 +60,38 @@ func main() {
 		"err": err,
 	}).Infof("2nd time get from cache")
 
+	/////////////// test blob ///////////////
+
+	type info struct {
+		Name string
+		Age  int
+	}
+
+	key2 := "some_key_yyy"
+	val := info{
+		Name: "tenz",
+		Age:  18,
+	}
+	err = cacheMgr.GetBlob(context.Background(), key2, val)
+	logger.WithFields(logger.Fields{
+		"key": key2,
+		"err": err,
+	}).Infof("1st time get blob")
+
+	err = cacheMgr.SetBlob(context.Background(), key2, val, 5*time.Second)
+	logger.WithFields(logger.Fields{
+		"key": key2,
+		"err": err,
+	}).Infof("set blob")
+
+	newVal := info{}
+	err = cacheMgr.GetBlob(context.Background(), key2, &newVal)
+	logger.WithFields(logger.Fields{
+		"key": key2,
+		"val": newVal,
+		"err": err,
+	}).Infof("2nd time get blob")
+
 	time.Sleep(100 * time.Millisecond)
 
 }
